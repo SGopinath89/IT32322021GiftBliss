a7facb6558ccbf68a74995b1bcdf8817f74942d7
SajathMH <87090630+SajathMH@users.noreply.github.com>
2024-05-09 22:44:21 +0530

    Add files via upload

---
+++ GiftBliss/Backend/GiftBliss/src/main/java/GB/GiftBlissApplication.java
@@ -0,0 +1,13 @@
+package GB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftBlissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftBlissApplication.class, args);
+	}
+
+}
