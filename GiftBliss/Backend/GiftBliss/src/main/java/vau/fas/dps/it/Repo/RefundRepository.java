e257dd4fb4d29289af1cf69604da2cd7d6a781b8
SajathMH <sajathmohamath272@gmail.com>
2024-06-12 14:08:58 +0530

    Admin add product page connect with backend

--- GiftBliss/Backend/GiftBliss/src/main/java/vau/fas/dps/it/Repo/RefundRepository.java
+++
@@ -1,8 +0,0 @@
-package vau.fas.dps.it.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vau.fas.dps.it.model.Refund;
-
-public interface RefundRepository extends JpaRepository<Refund, Long> {
-    // Custom methods can be added if needed
-}
--- GiftBliss/Frontend/giftbliss/src/App.js
+++ GiftBliss/Frontend/giftbliss/src/App.js
@@ -5,15 +5,17 @@ import CategoryPage from './Components/CategoryPage';
 import './CSS/HomePage.css';
 import './CSS/UserHeader.css';
 import './CSS/Footer.css';
+import AdminAddProduct from './Components/AdminAddProduct';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<AdminAddProduct />} />
+       {/*<Route path="/" element={<HomePage />} />
         <Route path="/:category" element={<CategoryPage />} />
-        <Route path="/:category/:subcategory" element={<CategoryPage />} />
+        <Route path="/:category/:subcategory" element={<CategoryPage />} />*/} 
       </Routes>
     </Router>
   );
