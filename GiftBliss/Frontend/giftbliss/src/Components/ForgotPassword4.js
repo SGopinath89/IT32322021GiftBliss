f0773fe79fb9069f7338c38fee2d9ca62023911a
Nuzha MeeraLebbe <152083956+NuzhaMeeraLebbe@users.noreply.github.com>
2024-06-30 22:36:12 +0530

    ForgotPassword4.js

--- GiftBliss/Frontend/giftbliss/src/Components/ForgotPassword4.js
+++ GiftBliss/Frontend/giftbliss/src/Components/ForgotPassword4.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/ForgotPassword4.css'; // Import the CSS file
+import '../CSS/ForgotPassword4.css'; // Import the CSS file
 
 const ForgotPassword4 = () => {
   const navigate = useNavigate();
