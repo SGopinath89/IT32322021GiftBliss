d1309ce3c012b325d847fdff8ae8496f0ab08498
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-07-07 12:11:46 +0530

    Create ResponseDto

---
+++ GiftBliss/Backend/GiftBliss/src/main/java/vau/fas/dps/it/dto/ResponseDto.java
@@ -0,0 +1,30 @@
+package vau.fas.dps.it.dto;
+
+public class ResponseDto {
+
+	private String status;
+	private String message;
+	
+	public ResponseDto(String status, String message) {
+		super();
+		this.status = status;
+		this.message = message;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
