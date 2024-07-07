5cb13598101d86f956186283d33a784f704ccf26
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-07-07 12:23:00 +0530

    Create SignupDto

---
+++ GiftBliss/Backend/GiftBliss/src/main/java/vau/fas/dps/it/dto/user/SignupDto.java
@@ -0,0 +1,29 @@
+package vau.fas.dps.it.dto.user;
+
+public class SignupDto {
+
+	private String fullname;
+	private String email;
+	private String password;
+	
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
