c3dc07c09d9d42e2bf5f3dce16a900dfc4399d83
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-07-07 23:39:51 +0530

    Item

---
+++ GiftBliss/Backend/GiftBliss/src/main/java/vau/fas/dps/it/model/Item.java
@@ -0,0 +1,50 @@
+package vau.fas.dps.it.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    private String name;
+    private double price;
+    
+	public Item(Long id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+    
+}
