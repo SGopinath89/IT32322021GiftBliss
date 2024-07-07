34ebb07d28e904868dd256603469e59b8fa212ec
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-07-07 12:10:37 +0530

    Create UserController

---
+++ GiftBliss/Backend/GiftBliss/src/main/java/vau/fas/dps/it/Controller/UserController.java
@@ -0,0 +1,34 @@
+package vau.fas.dps.it.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vau.fas.dps.it.Service.UserService;
+import vau.fas.dps.it.dto.ResponseDto;
+import vau.fas.dps.it.dto.user.SignInDto;
+import vau.fas.dps.it.dto.user.SigninResponseDto;
+import vau.fas.dps.it.dto.user.SignupDto;
+
+@RequestMapping("user")
+@RestController
+public class UserController {
+
+	@Autowired
+	UserService userService;
+	
+	
+	//signup
+	@PostMapping("/signup")
+	public ResponseDto signup(@RequestBody SignupDto signupDto) {
+		return userService.signUp(signupDto);
+	}
+	
+	//signin
+	@PostMapping("signin")
+	public SigninResponseDto signIn(@RequestBody SignInDto signInDto){
+		return userService.signIn(signInDto);
+	}
+}
