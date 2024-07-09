390ae65d73f10932e3ad28529218493d66d017c7
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-07-09 17:05:32 +0530

    Update PaymentService

--- GiftBliss/Backend/GiftBliss/src/main/java/vau/fas/dps/it/Services/PaymentService.java
+++ GiftBliss/Backend/GiftBliss/src/main/java/vau/fas/dps/it/Services/PaymentService.java
@@ -1,5 +1,6 @@
 package vau.fas.dps.it.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import vau.fas.dps.it.model.Payment;
@@ -8,6 +9,7 @@ import vau.fas.dps.it.Repo.PaymentRepository;
 @Service
 public class PaymentService {
 
+	@Autowired
 	private PaymentRepository paymentRepository;
 	
 	public Payment processPayment(Payment payment) {
